418d4e71fa2922c08fa0d4a4cdb46ea4f661358b
pji0219 <pji219@naver.com>
2022-02-03 00:44:31 +0900

    모달 컴포넌트 완성

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -1,7 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from '@emotion/styled/macro';
+
+// 스타일
+const ModalBackground = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ModalContainer = styled.div`
+  background: white;
+  padding: 24px 16px;
+  border-radius: 4px;
+  width: 320px;
+`;
+
+const ModalCloseBtnContainer = styled.div`
+  text-align: right;
+`;
+const ModalCloseBtn = styled.button`
+  width: 70px;
+  height: 35px;
+  border-radius: 4px;
+  border: none;
+  outline: none;
+  background: #228be6;
+  color: white;
+  cursor: pointer;
+`;
+
+const ModalTitle = styled.h2``;
+
+const ModalContent = styled.p`
+  font-size: 18px;
+`;
+
+const ModalOpenBtnContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const ModalOpenBtn = styled.button`
+  width: 100px;
+  height: 50px;
+  border-radius: 4px;
+  border: none;
+  outline: none;
+  background: #228be6;
+  color: white;
+  cursor: pointer;
+  font-size: 18px;
+`;
 
 function Modal() {
-  return <div></div>;
+  // 모달창을 닫고 열기 위한 state
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <>
+      {showModal ? (
+        <ModalBackground>
+          <ModalContainer>
+            <ModalCloseBtnContainer>
+              <ModalCloseBtn onClick={closeModal}>닫기</ModalCloseBtn>
+            </ModalCloseBtnContainer>
+            <ModalTitle>안녕하세요!</ModalTitle>
+            <ModalContent>반갑습니다😊</ModalContent>
+          </ModalContainer>
+        </ModalBackground>
+      ) : (
+        <ModalOpenBtnContainer>
+          <ModalOpenBtn onClick={openModal}>모달 열기</ModalOpenBtn>
+        </ModalOpenBtnContainer>
+      )}
+    </>
+  );
 }
 
 export default Modal;
