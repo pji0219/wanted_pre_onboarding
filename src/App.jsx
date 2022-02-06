9e943a388e6c6477cbc85826ddb02be4e107df8e
pji0219 <pji219@naver.com>
2022-02-07 01:28:57 +0900

    토글 컴포넌트 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Modal from './component/Modal';
-import Tab from './component/tab/Tab';
+import Tab from './component/Tab';
+import Toggle from './component/Toggle';
 
 function App() {
   return (
     <>
       <Tab />
+      <Toggle />
       <Modal />
     </>
   );
---
+++ src/component/Toggle.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled/macro';
+
+// 스타일
+const ToggleContainer = styled.div`
+  position: fixed;
+  top: 20px;
+  left: 40%;
+  width: 75px;
+  height: 35px;
+  cursor: pointer;
+
+  > .toggle {
+    width: 100%;
+    height: 100%;
+    border-radius: 30px;
+    background-position: right;
+    background: linear-gradient(to left, #8b8b8b 50%, #228be6 50%) right;
+    background-size: 200%;
+    transition: 1s;
+
+    &.toggle-on {
+      background-position: left;
+      background: linear-gradient(to right, #228be6 50%, #8b8b8b 50%) left;
+      background-size: 200%;
+      transition: 1s;
+    }
+  }
+
+  > .toggle-ball {
+    position: absolute;
+    top: 3px;
+    left: 3px;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    background-color: #fff;
+    transition: 1s;
+
+    &.toggle-on {
+      left: 41px;
+      transition: 1s;
+    }
+  }
+`;
+
+const ToggleStatus = styled.div`
+  position: fixed;
+  top: 60px;
+  left: 40%;
+  display: flex;
+  justify-content: center;
+`;
+
+function Toggle() {
+  const [toggleOn, setToggleOn] = useState(false);
+
+  const toggleHandler = () => {
+    setToggleOn(!toggleOn);
+  };
+
+  return (
+    <>
+      <ToggleContainer onClick={toggleHandler}>
+        <div className={`toggle ${toggleOn ? 'toggle-on' : ''}`}></div>
+        <div className={`toggle-ball ${toggleOn ? 'toggle-on' : ''}`}></div>
+      </ToggleContainer>
+      <ToggleStatus>{toggleOn ? '토글 on' : '토글 off'}</ToggleStatus>
+    </>
+  );
+}
+
+export default Toggle;
